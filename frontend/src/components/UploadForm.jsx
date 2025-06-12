50ccc2bc3a5a964e3062449747d7d25858cd591d
lpfe <pcheol0311@gmail.com>
2025-06-12 02:10:35 +0000

    경로수정

--- frontend/src/components/UploadForm.jsx
+++ frontend/src/components/UploadForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios'
-import './styles/UploadForm.css'
+import '/work/BarunChuck/frontend/src/styles/UploadForm.css'
 
 function UploadForm() {
   const [file, setFile] = useState(null)
