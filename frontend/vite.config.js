dd877bb2939d8a85d43a3b70fcde089ee9696ebe
lpfe <pcheol0311@gmail.com>
2025-06-19 07:02:26 +0000

    resultpage_mp4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/team-backend/', // ← 이 줄 추가
   plugins: [react()],
   server: {
     proxy: {
