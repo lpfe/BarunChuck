e8d1592b98586e1505a38a506e99fe76b65479a4
lpfe <pcheol0311@gmail.com>
2025-06-25 04:29:25 +0000

    autoplay_muted

--- frontend/src/components/ResultPage.jsx
+++ frontend/src/components/ResultPage.jsx
@@ -32,7 +32,7 @@ function ResultPage() {
       {/* ✅ 좌측: 키포인트 찍힌 영상 */}
       <div className="video-box">
         <h3>자세 영상</h3>
-        <video controls>
+        <video controls autoPlay muted width="100%" style={{ backgroundColor: 'black' }}>
           <source src={`http://56.155.62.180:8000/getDrawnVideo/${filename}.mp4`} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
