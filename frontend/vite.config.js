27adbee0d54ac95cce2f75bd8cf082f5ab58e6b7
lpfe <pcheol0311@gmail.com>
2025-06-25 06:35:40 +0000

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     proxy: {
-      '/uploadFile': 'http://localhost:8000',
+      '/uploadFile': 'http://barunchuck.5team.store:8000',
     },
   },
 })
